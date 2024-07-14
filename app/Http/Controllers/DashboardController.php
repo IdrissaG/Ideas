b60454fb141262f7bb5b4cc2281810d3b391abb0
IdrissaG <idrissa.gueye23@protonmail.com>
2024-07-14 16:48:07 +0100

    Initial commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\idea;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        $idea = new idea([
+            'content'=>'Test',
+        ]);
+
+        $idea->save();
+        dump(Idea::all());
+
+        return view('Dashboard', [
+            'ideas' => Idea::all()
+        ]);
+
+    }
+}
